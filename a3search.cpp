b1545284f87cb1f6b267a526a870bededdb4b446
awszwc <xlb25@126.com>
2017-05-06 17:13:06 +1000

    9319 Ass3 start

---
+++ a3search.cpp
@@ -0,0 +1,15 @@
+//
+//  a3search.cpp
+//  a3search
+//
+//  Created by 张云翮 on 06/05/2017.
+//  Copyright © 2017 Zhang Yunhe. All rights reserved.
+//
+
+#include "a3search.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ a3search.h
@@ -0,0 +1,15 @@
+//
+//  a3search.h
+//  a3search
+//
+//  Created by 张云翮 on 06/05/2017.
+//  Copyright © 2017 Zhang Yunhe. All rights reserved.
+//
+
+#ifndef a3search_h
+#define a3search_h
+
+#include <stdio.h>
+#include <iostream>
+
+#endif /* a3search_h */
