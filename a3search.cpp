31e62c7a772ec38d3a176f97c83ca41c1613265c
awszwc <xlb25@126.com>
2017-05-22 20:41:43 +1000

    abc

--- a3search.cpp
+++ a3search.cpp
@@ -209,7 +209,7 @@ void build_index(const char * argument1, const char * argument2) {
                 continue;
             }
             entry = readdir(pDIR);
-            while(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".DS_Store") == 0) {
+            while(strcmp(entry->d_name, ".") == 0 or strcmp(entry->d_name, "..") == 0 or strcmp(entry->d_name, ".DS_Store") == 0) {
                 entry = readdir(pDIR);
                 cout<<entry->d_name<<endl;
             }
